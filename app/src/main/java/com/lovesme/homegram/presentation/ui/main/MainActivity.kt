05ae1294501b182764be722fab7d8e34759b76d9
Kwon Myeongeun <kmyeongeun95@gmail.com>
2023-03-02 18:49:10 +0900

    [Design] 메인 navigation, 플레그먼트 추가

---
+++ app/src/main/java/com/lovesme/homegram/presentation/ui/main/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.lovesme.homegram.presentation.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.lovesme.homegram.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navController =
+            supportFragmentManager.findFragmentById(binding.homeFrmContainer.id)
+                ?.findNavController()
+        navController?.let { binding.homeBtmNavigation.setupWithNavController(it) }
+    }
+}
