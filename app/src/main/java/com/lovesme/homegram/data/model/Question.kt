8c0fad97c49253ef6c0b98d77a70ac8c894c310a
Kwon Myeongeun <kmyeongeun95@gmail.com>
2023-03-10 20:13:28 +0900

    [Design] 답변 화면 추가

--- app/src/main/java/com/lovesme/homegram/data/model/Question.kt
+++ app/src/main/java/com/lovesme/homegram/data/model/Question.kt
@@ -1,7 +1,12 @@
 package com.lovesme.homegram.data.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Question(
     val seq: String?,
     val contents: String?,
     val answer: List<Answer>? = null
-)
+) : Parcelable
+
--- app/src/main/java/com/lovesme/homegram/presentation/ui/main/daily/DailyFragment.kt
+++ app/src/main/java/com/lovesme/homegram/presentation/ui/main/daily/DailyFragment.kt
@@ -1,22 +1,26 @@
 package com.lovesme.homegram.presentation.ui.main.daily
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import com.lovesme.homegram.data.model.Question
+import com.lovesme.homegram.data.model.listener.QuestionClickListener
 import com.lovesme.homegram.databinding.FragmentDailyBinding
 import com.lovesme.homegram.presentation.ui.viewmodel.DailyViewModel
+import com.lovesme.homegram.util.Constants
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class DailyFragment : Fragment() {
+class DailyFragment : Fragment(), QuestionClickListener {
 
     private var _binding: FragmentDailyBinding? = null
     private val binding get() = _binding!!
     private val dailyViewModel: DailyViewModel by activityViewModels()
-    private val adapter = DailyQuestionRVAdapter()
+    private val adapter = DailyQuestionRVAdapter(this)
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -34,5 +38,11 @@ class DailyFragment : Fragment() {
         dailyViewModel.questions.observe(viewLifecycleOwner) { question ->
             adapter.submitList(question.toMutableList())
         }
+    }
+
+    override fun onClickItem(question: Question) {
+        val intent = Intent(context, DailyDetailActivity::class.java)
+        intent.putExtra(Constants.PARCELABLE_QUESTION, question)
+        startActivity(intent)
     }
 }
