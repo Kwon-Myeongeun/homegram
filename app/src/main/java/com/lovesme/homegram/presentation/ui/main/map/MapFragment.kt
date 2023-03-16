df3a30a9eaf82cca51b8b4784ec3e07d1a0f7dd1
Kwon Myeongeun <kmyeongeun95@gmail.com>
2023-03-17 04:03:00 +0900

    [Feat] 구글맵 추가

--- app/src/main/java/com/lovesme/homegram/presentation/ui/main/map/MapFragment.kt
+++ app/src/main/java/com/lovesme/homegram/presentation/ui/main/map/MapFragment.kt
@@ -5,12 +5,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.lovesme.homegram.databinding.FragmentMapBinding
 
-class MapFragment : Fragment() {
+
+class MapFragment : Fragment(), OnMapReadyCallback {
 
     private var _binding: FragmentMapBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var mapView: MapView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -18,6 +28,53 @@ class MapFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentMapBinding.inflate(inflater, container, false)
+        mapView = binding.mapView
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this)
         return binding.root
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+
+        val HOME = LatLng(37.635731, 126.869626)
+
+        val markerOptions = MarkerOptions()
+            .position(HOME)
+            .title("집")
+            .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET))
+
+        map.addMarker(markerOptions)
+
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(HOME, 10f))
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+    override fun onDestroy() {
+        mapView.onDestroy()
+        super.onDestroy()
     }
 }
