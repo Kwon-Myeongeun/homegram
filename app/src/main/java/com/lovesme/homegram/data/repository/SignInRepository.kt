3949303f9611028ad73676ed078763dfa8a7bc02
Kwon Myeongeun <kmyeongeun95@gmail.com>
2023-03-16 21:15:28 +0900

    [Feat] 불필요한 fun 삭제

--- app/src/main/java/com/lovesme/homegram/data/repository/SignInRepository.kt
+++ app/src/main/java/com/lovesme/homegram/data/repository/SignInRepository.kt
@@ -5,5 +5,4 @@ import com.lovesme.homegram.data.model.Result
 interface SignInRepository {
     suspend fun saveLogInUserInfo(): Result<Unit>
     suspend fun joinToInvitedGroup(groupId: String): Result<Unit>
-    suspend fun updateUserInfo(groupId: String, name: String, birth: String): Result<Unit>
 }
