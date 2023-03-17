1f66770460231e79151ca8a111e14e8bdd512365
Kwon Myeongeun <kmyeongeun95@gmail.com>
2023-03-17 18:56:29 +0900

    [Feat] 위치 정보 가져오기

--- app/src/main/java/com/lovesme/homegram/util/location/LocationService.kt
+++ app/src/main/java/com/lovesme/homegram/util/location/LocationService.kt
@@ -1,13 +1,22 @@
 package com.lovesme.homegram.util.location
 
 import android.Manifest
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Looper
 import android.util.Log
 import androidx.core.app.ActivityCompat
 import androidx.lifecycle.LifecycleService
 import com.google.android.gms.location.*
 import com.lovesme.homegram.data.model.Location
+import com.lovesme.homegram.presentation.ui.main.MainActivity
+import com.lovesme.homegram.R
 
 class LocationService : LifecycleService() {
     private val locationRequest by lazy {
@@ -16,12 +25,44 @@ class LocationService : LifecycleService() {
     private val fusedLocationClient by lazy {
         LocationServices.getFusedLocationProviderClient(this)
     }
+    private val notificationManager by lazy {
+        this.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+    }
     private lateinit var userLocation: Location
     private lateinit var locationCallback: LocationCallback
 
     override fun onCreate() {
         super.onCreate()
         getPersonLocation()
+        setForeground()
+    }
+
+    private fun setForeground() {
+        createNotification()
+        getPersonLocation()
+
+    }
+
+    private fun createNotification() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val pendingIntent: PendingIntent =
+                Intent(this, MainActivity::class.java).let { notificationIntent ->
+                    PendingIntent.getActivity(
+                        this, 0, notificationIntent,
+                        PendingIntent.FLAG_IMMUTABLE
+                    )
+                }
+            val channelId = applicationContext.getString(R.string.mission_notification_channel_id)
+            val notification: Notification = Notification.Builder(this, channelId)
+                .setContentTitle(applicationContext.getString(R.string.notification_title))
+                .setContentText(applicationContext.getString(R.string.notification_message))
+                .setSmallIcon(R.drawable.pet_normal_origin)
+                .setContentIntent(pendingIntent)
+                .setTicker(applicationContext.getString(R.string.notification_title))
+                .build()
+            createChannel(channelId)
+            startForeground(82, notification)
+        }
     }
 
     private fun getPersonLocation() {
@@ -41,7 +82,7 @@ class LocationService : LifecycleService() {
                 if (location != null) {
                     userLocation = Location(location.latitude, location.longitude)
 
-                    Log.d("Location", "${userLocation.latitude.toString()} , ${userLocation.longitude.toString()}")
+                    Log.d("Location", "${userLocation.latitude} , ${userLocation.longitude}")
                 }
             }
             .addOnFailureListener {
@@ -54,7 +95,7 @@ class LocationService : LifecycleService() {
                     if (location != null) {
                         userLocation = Location(location.latitude, location.longitude)
 
-                        Log.d("Location", "${userLocation.latitude.toString()} , ${userLocation.longitude.toString()}")
+                        Log.d("Location", "${userLocation.latitude} , ${userLocation.longitude}")
                     }
                 }
             }
@@ -64,6 +105,17 @@ class LocationService : LifecycleService() {
             locationCallback,
             Looper.getMainLooper()
         )
+    }
+
+    private fun createChannel(id: String) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            if (notificationManager.getNotificationChannel(id) == null) {
+                val name = this.getString(R.string.mission_notification_channel_name)
+                NotificationChannel(id, name, NotificationManager.IMPORTANCE_DEFAULT).apply {
+                    notificationManager.createNotificationChannel(this)
+                }
+            }
+        }
     }
 
     companion object {
