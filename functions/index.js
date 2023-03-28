48824845d5d806e0236cf35238f6393b114acc2b
Kwon Myeongeun <kmyeongeun95@gmail.com>
2023-03-28 22:32:44 +0900

    [Feat] 매일 질문 갱신하는 firebase function 추가

--- functions/index.js
+++ functions/index.js
@@ -4,9 +4,26 @@ admin.initializeApp(functions.config().firebase);
 
 var database = admin.database();
 
-exports.dailyScheduledFunction = functions.pubsub.schedule("every 1 minutes")
+var dailyRef = database.ref("daily/");
+
+exports.dailyContentScheduledFunction = functions.pubsub.schedule("0 9 * * *")
     .timeZone("Asia/Seoul")
     .onRun(async (context) => {
-        database.ref("test/").set("testDate");
+        dailyRef.once("value").then(snapshot => {
+            snapshot.forEach(group => {
+                var groupRef = database.ref("daily/" + group.key + "/");
+                groupRef.orderByChild("no").limitToLast(1).once("value").then(maxGroup => {
+                    maxGroup.forEach(function(data) {
+                        var isDoneVal = data.child("isDone").val();
+                        if(isDoneVal){
+                            var nextVal = parseInt(data.child("no").val()) + 1;
+                            database.ref("question/" + nextVal + "/").once("value").then(newContents => {
+                                groupRef.push().set({ no: nextVal, isDone: false, contents: newContents.val() })
+                            });
+                        }
+                    });
+                });
+            });
+        });
         return null;
     });
