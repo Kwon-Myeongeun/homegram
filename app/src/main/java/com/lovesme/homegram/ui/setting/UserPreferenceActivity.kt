1abe1fd55409fb9b2e2de3f2c7bd6e36a122de62
Kwon Myeongeun <kmyeongeun95@gmail.com>
2023-04-09 16:45:03 +0900

    [Feat] 생일 입력 UI 수정

--- app/src/main/java/com/lovesme/homegram/ui/setting/UserPreferenceActivity.kt
+++ app/src/main/java/com/lovesme/homegram/ui/setting/UserPreferenceActivity.kt
@@ -1,24 +1,42 @@
 package com.lovesme.homegram.ui.setting
 
+import android.app.DatePickerDialog
 import android.content.Intent
 import android.os.Bundle
+import android.widget.DatePicker
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doAfterTextChanged
 import com.lovesme.homegram.databinding.ActivityUserpreferenceBinding
 import com.lovesme.homegram.ui.main.MainActivity
 import com.lovesme.homegram.ui.viewmodel.UserPreferenceViewModel
+import com.lovesme.homegram.util.DateFormatText
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
-class UserPreferenceActivity : AppCompatActivity() {
+class UserPreferenceActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener {
 
     private lateinit var binding: ActivityUserpreferenceBinding
     private val userPreferenceViewModel: UserPreferenceViewModel by viewModels()
+
+    @Inject
+    lateinit var dateFormatText: DateFormatText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityUserpreferenceBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.birthSettingTv.setOnClickListener {
+            DatePickerDialog(
+                this,
+                this,
+                dateFormatText.getTodayYEAR(),
+                dateFormatText.getTodayMONTH(),
+                dateFormatText.getTodayDATE(),
+            ).show()
+        }
 
         binding.saveBtn.setOnClickListener {
             userPreferenceViewModel.updateUserInfo(
@@ -27,6 +45,17 @@ class UserPreferenceActivity : AppCompatActivity() {
             )
             startActivity(Intent(this, MainActivity::class.java))
             finish()
+        }
+
+        binding.nameSettingTv.doAfterTextChanged {
+            binding.saveBtn.isEnabled = !it.isNullOrEmpty() && !binding.birthSettingTv.text.isNullOrEmpty()
+        }
+    }
+
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+        binding.birthSettingTv.setText("${year}.${month}.${dayOfMonth}")
+        if (!binding.nameSettingTv.text.isNullOrEmpty()) {
+            binding.saveBtn.isEnabled = true
         }
     }
 }
